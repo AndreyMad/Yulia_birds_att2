27a8bc8686050bdcf0e09754be7d275edd27a072
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2019-10-04 19:26:20 +0300

    fix git

--- js/more.js
+++ js/more.js
@@ -1,14 +1,15 @@
 const items = [
-    {
-        name: 'feeder_house',
-        imgSrc: './img/yellow_house.jpg',
-        about: `Кормушка "Теремок" изготовлена из литого акрилового стекла производ-
+  {
+    
+    name: "feeder_house",
+    imgSrc: "./img/yellow_house.jpg",
+    about: `Кормушка "Теремок" изготовлена из литого акрилового стекла производ-
         ства Германии. Это кристально прозрачный материал с длительным сроком 
         эксплуатации. В кормушке установлена деревянная деталь для удобной по-
         садки птиц.`,
-        volume: ' 0,2 л. корма',
-        size: '150х130х50 мм.',
-        aboutText: `Для крепления на стекло используются качественные, большие прозрачные 
+    volume: " 0,2 л. корма",
+    size: "150х130х50 мм.",
+    aboutText: `Для крепления на стекло используются качественные, большие прозрачные 
         присоски, хорошо держатся при любой температуре (проверено много раз, 
         держатся всю зиму в мороз, снег, дождь). Но даже если вы беспокоитесь, что 
         кормушка может отпасть и улететь, предусмотрена система "Якорь" (страхо-
@@ -17,48 +18,42 @@ const items = [
         правильном обращении имеют длительный  срок службы  и требуют минима-
         льного ухода. Если вы хотите рассматривать и фотографировать птиц без 
         помех, то акриловые кормушки для вас!`
-    },
-    {
-        name: 'feeder_photo',
-        about: `Кормушка "Теремок" изготовлена из литого акрилового стекла производ-
+  },
+  {
+    name: "feeder_photo",
+    about: `Кормушка "Теремок" изготовлена из литого акрилового стекла производ-
         ства Германии. Это кристально прозрачный материал с длительным сроком 
         эксплуатации. В кормушке установлена деревянная деталь для удобной по-
         садки птиц.`,
-        volume: ' 0,2 л. корма',
-        size: '150х130х50 мм.',
+    volume: " 0,2 л. корма",
+    size: "150х130х50 мм.",
 
+    aboutText: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis mollitia nam dicta! Voluptas quidem nisi animi culpa expedita accusamus? Nisi est exercitationem ipsam, consequatur beatae voluptas ullam facilis facere? Neque sit accusantium debitis quis a quae consectetur iusto, ut voluptate omnis eaque corporis, rerum, quasi id harum perferendis quo corrupti?`
+  },
+  {
+    name: "feeder_lodgy"
+  },
+  {
+    name: "feeder_teremok"
+  },
+  {
+    name: "feeder_flamingo"
+  }
+];
 
-        aboutText: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis mollitia nam dicta! Voluptas quidem nisi animi culpa expedita accusamus? Nisi est exercitationem ipsam, consequatur beatae voluptas ullam facilis facere? Neque sit accusantium debitis quis a quae consectetur iusto, ut voluptate omnis eaque corporis, rerum, quasi id harum perferendis quo corrupti?`
-    },
-    {
-        name: 'feeder_lodgy'
-    },
-    {
-        name: 'feeder_teremok'
-    },
-    {
-        name: 'feeder_flamingo'
-    },
-    
-    
-]
+function htmlCreate(event) {
+  if (event.target.classList == "feeder_details") {
+    const outWrapper = event.target.closest(".feeder_out_wrapper");
+    let overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+    let innerWrapper = document.createElement("div");
+    innerWrapper.classList.add("modal_innerWrapper");
+    let img =document.createElement('img');
 
-
-function htmlCreate(event){
-if(event.target.classList == 'feeder_details'){
-    const outWrapper = (event.target.closest('.feeder_out_wrapper') );
-    let overlay = document.createElement('div')
-    overlay.classList.add('overlay')
-    let innerWrapper = document.createElement('div')
-    innerWrapper.classList.add('modal_innerWrapper')
-
-
-
-    outWrapper.prepend(overlay)
-    overlay.append(innerWrapper)
-         
-}
+   
+    outWrapper.prepend(overlay);
+    overlay.append(innerWrapper);
+  }
 }
 
-
-window.addEventListener('click', htmlCreate)
+window.addEventListener("click", htmlCreate);
